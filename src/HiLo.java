493a204b7cd2a2a963f18d595543dde453b61249
Ivan Tarskij <tarskijivan@gmail.com>
2024-03-24 16:10:25 +0400

    добавил подсчет попыток

--- src/HiLo.java
+++ src/HiLo.java
@@ -5,23 +5,30 @@ public class HiLo {
         Scanner in = new Scanner(System.in);
         String playAgain;
         do {
-            // Сгенерировать случайное число от 1 до 100, которое пользователь будет угадывать.
-            int number = (int) (Math.random() * 100 + 1);
+            int number = (int) (Math.random() * 100 + 1); // Сгенерировать случайное число от 1 до 100, которое пользователь будет угадывать.
             // Вывести на экран строку текста (запрос), попросив пользователя угадать число в указанном диапазоне.
-            int guess = 0;
             // Просить пользователя дать ответ до тех пор, пока он не угадает число.
+            int guess = 0;
+            int numberOfTries = 0;
+
             while (guess != number) {
                 System.out.println("Введите число между 1 и 100: ");
                 guess = in.nextInt();
                 // Сравнить ответ пользователя с загаданным числом.
                 // Определить их соотношение: больше, меньше или равно.
+                // Отобразить результат сравнения на экране.
                 if (guess < number)
-                    System.out.println(guess + " меньше." + "Попробуйте снова."); // Отобразить результат сравнения на экране.
+                    System.out.println(guess + " меньше." + "Попробуйте снова.");
                 else if (guess > number)
                     System.out.println(guess + " больше." + "Попробуйте снова.");
                 else
                     System.out.println(guess + " правильно." + " Вы выиграли!");
+                numberOfTries = numberOfTries + 1;
             }
+
+            if (guess == number)
+                System.out.println("Вам потребовалось всего " + numberOfTries + " попыток! Хорошая работа!");
+
             // Спросить пользователя, хочет ли он снова сыграть в игру.
             System.out.println("Хотите снова сыграть в игру? (y/n)");
             playAgain = in.next();
